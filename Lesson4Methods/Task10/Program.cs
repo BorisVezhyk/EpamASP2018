0949626c4997c2470cd83df0a7113e36f734a742
BorisVezhyk <randomar9@gmail.com>
2018-10-07 17:54:53 +0300

    Lesson4Methods
    Lesson4 Methods
    Task10 and Task11

---
+++ Lesson4Methods/Task10/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Security.Principal;
+
+namespace Task10
+{
+	class Program
+	{
+		private const int MaxRangeFactorial = 100;
+		private const int MaxSizeArray = 1000;
+		private const int LimitOfCell = 9;
+
+		static void Main(string[] args)
+		{
+			int[] arrayFactorial = new int[MaxSizeArray];
+			arrayFactorial[MaxSizeArray-1] = 1;
+			for (int i = 1; i <= MaxRangeFactorial; i++)
+			{
+				arrayFactorial= GetArrayFact(arrayFactorial,i);
+			}
+			PrintArrayFactorial(arrayFactorial);
+			Console.ReadLine();
+		}
+
+		static int[] GetArrayFact(int[] sourceArr, int factorial)
+		{
+			int[] arrayResultFact = sourceArr;
+			int multiplier = factorial;
+			int remainder = 0;
+
+			for (int i = arrayResultFact.Length-1; i >=0; i--)
+			{
+				int total = arrayResultFact[i] * multiplier+remainder;
+				remainder = 0;
+				if (total > LimitOfCell)
+				{
+					arrayResultFact[i] = total % 10;
+					remainder = total / 10;
+				}
+				else
+				{
+					arrayResultFact[i] = total;
+				}
+			}
+
+			return arrayResultFact;
+		}
+
+		static void PrintArrayFactorial(int[] arrayFactorial)
+		{
+			bool isFirstZeros = true;
+			for (int i = 0; i < arrayFactorial.Length; i++)
+			{
+				if (isFirstZeros && arrayFactorial[i]==0)
+				{
+					continue;
+				}
+
+				isFirstZeros = false;
+				Console.Write(arrayFactorial[i]);
+			}
+
+			Console.WriteLine();
+		}
+	}
+
+	
+}
---
+++ Lesson4Methods/Task11/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Text;
+
+namespace TestRiht
+{
+	class Program
+	{
+
+		static void Main(string[] args)
+		{
+			int[] arrPoly = GetArrayForPolynomial();
+			string polynomial= GetStringPolynomial(arrPoly);
+			Console.WriteLine(polynomial);
+			Console.ReadLine();
+		}
+
+		static string GetStringPolynomial(params int[] arrPoly)
+		{
+			StringBuilder result=new StringBuilder();
+			for (int i = arrPoly.Length-1; i >=0; i--)
+			{
+
+				if (arrPoly[i] == 0) {continue;}
+				if (FirstPart(arrPoly, i, result)) {continue;}
+				NeedToAddPlus(arrPoly, i, result);
+				if (PreLastPart(arrPoly, i, result)) {continue;}
+				if (LastPart(arrPoly, i, result)) {continue;}
+				result.Append(arrPoly[i]+"x^"+i);
+				
+			}
+
+			return result.ToString();
+		}
+
+		private static void NeedToAddPlus(int[] arrPoly, int i, StringBuilder result)
+		{
+			if (arrPoly[i] > 0 && result.Length > 0)
+			{
+				result.Append("+");
+			}
+		}
+
+		private static bool FirstPart(int[] arrPoly, int i, StringBuilder result)
+		{
+			if (i == arrPoly.Length - 1 && i > 1)
+			{
+				result.Append(GetFirstPartStringPoly(arrPoly, i, result.ToString()));
+				return true;
+			}
+
+			return false;
+		}
+
+		private static bool LastPart(int[] arrPoly, int i, StringBuilder result)
+		{
+			if (i == 0)
+			{
+				result.Append(arrPoly[i]);
+				return true;
+			}
+
+			return false;
+		}
+
+		private static bool PreLastPart(int[] arrPoly, int i, StringBuilder result)
+		{
+			if (i == 1)
+			{
+				if (arrPoly[i]==1)
+				{
+					result.Append("x");
+					return true;
+				}
+				result.Append(arrPoly[i] + "x");
+				return true;
+
+			}
+
+			return false;
+		}
+
+		private static string GetFirstPartStringPoly(int[] arrPoly, int i, string res)
+		{
+			string result = res;
+			if (arrPoly[i] == 1)
+			{
+				result+="x^" + i;
+			}
+			else
+			{
+				result+=arrPoly[i] + "x^" + i;
+			}
+
+			return result;
+		}
+
+
+		static int[] GetArrayForPolynomial()
+		{
+			Console.WriteLine("Enter number for array length n>0");
+			int lengthArray = Convert.ToInt32(Console.ReadLine());
+			Console.WriteLine(string.Format("Enter {0} numbers",lengthArray));
+			int[] arrayForPoly = new int[lengthArray];
+			for (int i = 0; i < lengthArray; i++)
+			{
+				arrayForPoly[i] = Convert.ToInt32(Console.ReadLine());
+			}
+
+			return arrayForPoly;
+		}
+	}
+}
