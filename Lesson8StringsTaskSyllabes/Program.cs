50577c45d4e4d8c12d232682491cbe1536d7c95c
BorisVezhyk <randomar9@gmail.com>
2018-10-16 01:37:29 +0300

    TraditionalStudySyllabes

---
+++ Lesson8StringsTaskSyllabes/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text;
+
+namespace Less8TaskSyllabes
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.OutputEncoding = Encoding.UTF8;
+			string text =
+				"Спайка Прощай зайка спайка \n" +
+				" Семья Перья дарья момблан банька\n " +
+				"Я сразу смазал карту будня,\n " +
+				"плеснувши краску из стакана;\n " +
+				"я показал на блюде студня\n" +
+				" косые скулы океана.\n" +
+				" На чешуе жестяной рыбы\n" +
+				" прочёл я зовы новых губ.\n" +
+				" А вы\n" +
+				" ноктюрн сыграть\n" +
+				" могли бы\n" +
+				" на флейте водосточных труб?";
+			Console.WriteLine(text);
+			Syllabe syllabe=new Syllabe();
+			string[] words = syllabe.GetArrayWords(text);
+			syllabe.ConverWordsInSyllabes(words);
+			//text = string.Join(" ", words);
+			text = syllabe.CompileText(words);
+
+			Console.WriteLine("\nРазделяем слова текста на слоги по технике Традиционная школа\n");
+			Console.WriteLine(text);
+
+
+			Console.ReadLine();
+		}
+	}
+}
---
+++ Lesson8StringsTaskSyllabes/Syllabe.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace Less8TaskSyllabes
+{
+	class Syllabe
+	{
+		private char[] vowels =
+			{'ё', 'Ё', 'у', 'У', 'е', 'Е', 'ы', 'Ы', 'а', 'А', 'о', 'О', 'э', 'Э', 'я', 'Я', 'и', 'И', 'ю', 'Ю'};
+
+		private char[] excTraditionalStudy = {'й', 'ъ', 'ь'};
+
+		public string[] GetArrayWords(string text)
+		{
+			return text.Split(new []{" ","\r\n"},StringSplitOptions.None);
+		}
+
+		public string CompileText(string[] words)
+		{
+			return string.Join(" ", words);
+		}
+
+		public void ConverWordsInSyllabes(string[] words)
+		{
+			for (int i = 0; i < words.Length; i++)
+			{
+				int countVolwels = GetCountVolwels(words[i]);
+				if (countVolwels==1 && words[i].Length==1)
+				{
+					continue;
+				}
+
+				words[i] = GetStringWithSyllabes(words[i],countVolwels);
+			}
+		}
+
+		private string GetStringWithSyllabes(string word, int countVolwels)
+		{
+			StringBuilder result = new StringBuilder();
+			bool neededAppend = false;
+			bool isThereExcpt = CheckOnTraditionalStudy(word);
+			for (int i = 0; i < word.Length; i++)
+			{
+				neededAppend = true;
+				for (int j = 0; j < vowels.Length; j++)
+				{
+					if (word[i]==vowels[j] && countVolwels>1)
+					{
+						if ((i+1)<word.Length && excTraditionalStudy.Contains(word[i+1]))
+						{
+							result.Append(word.Substring(i,2)+"-");
+							i++;
+							neededAppend = false;
+							countVolwels--;
+							continue;
+						}
+						if((i+2)<word.Length && excTraditionalStudy.Contains(word[i + 2]))
+						{
+							result.Append(word.Substring(i, 3) + "-");
+							i += 2;
+							neededAppend = false;
+							countVolwels--;
+							continue;
+						}
+						else
+						{
+							result.Append(word[i] + "-");
+							neededAppend = false;
+							countVolwels--;
+							continue;
+						}
+					}
+				}
+
+				if (neededAppend)
+				{
+					result.Append(word[i]);
+				}
+			}
+
+			return result.ToString();
+		}
+
+		private int GetCountVolwels(string word)
+		{
+			return word.Split(vowels).Length - 1;
+		}
+
+		private bool CheckOnTraditionalStudy(string word)
+		{
+			return word.IndexOfAny(excTraditionalStudy) > 0;
+		}
+
+
+	}
+}
