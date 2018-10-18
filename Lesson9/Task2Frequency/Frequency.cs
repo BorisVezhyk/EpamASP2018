cf0c0c38885e8899eb71b623ed8956b7bb8330a3
BorisVezhyk <randomar9@gmail.com>
2018-10-18 10:24:38 +0300

    Task2FrequensyWords

---
+++ Lesson9/Task2Frequency/Frequency.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lesson9Task2
+{
+	static class Frequency
+	{
+		static Dictionary<string, int> frequencyInts = new Dictionary<string, int>();
+
+		private static string[] GetSplitText(string text)
+		{
+			text.Trim(' ');
+			return text.Split(new char[] {' ', '.', ',', '?', '!'});
+		}
+
+		public static void PrintFrequencyWordInText(string text)
+		{
+			frequencyInts.Clear();
+			string[] words = GetSplitText(text);
+			foreach (var word in words)
+			{
+				SaveInDictionary(word.ToLower());
+			}
+			Print();
+		}
+
+		private static void SaveInDictionary(string word)
+		{
+			
+			if (frequencyInts.ContainsKey(word))
+			{
+				frequencyInts[word] = frequencyInts[word] + 1;
+			}
+			else
+			{
+				frequencyInts.Add(word,1);
+			}
+		}
+
+		private static void Print()
+		{
+			foreach (var word in frequencyInts)
+			{
+				Console.WriteLine("Frequency {0} ={1}",word.Key,word.Value);
+			}
+		}
+
+	}
+}
---
+++ Lesson9/Task2Frequency/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Lesson9Task2
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string text =
+				"world wide web wide word big pig web hello world hi there next Level how are you you need to solve this task," +
+				" world wiDE web wide word big pig web Hello WORLD how are you you need to solve this";
+
+			Frequency.PrintFrequencyWordInText(text);
+
+			Console.ReadLine();
+		}
+	}
+}
