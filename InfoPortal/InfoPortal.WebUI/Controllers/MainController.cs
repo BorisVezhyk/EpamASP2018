229b75d613036da43e25db8b7c98fe69d227af8d
BorisVezhyk <randomar9@gmail.com>
2018-11-26 19:44:22 +0300

    Add Home View

---
+++ InfoPortal/InfoPortal.WebUI/Controllers/MainController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using InfoPortal.Domain.Abstract;
+using InfoPortal.Domain.Concrete;
+
+namespace InfoPortal.WebUI.Controllers
+{
+	public class MainController : Controller
+	{
+		IArticlesRepository _articles=new ArticleRepository();
+
+		public ActionResult Index()
+		{
+			
+			
+			return View(_articles.Articles.Take(6));
+		}
+		
+	}
+}
